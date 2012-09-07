6d864f36ac7dfb105d91e0c68fed37ea61837cbe
Andrey Kumanyaev <me@zzet.org>
2012-09-07 14:43:58 +0400

    repare gem

--- lib/http_accept_language/railtie.rb
+++ lib/http_accept_language/railtie.rb
@@ -1,5 +1,5 @@
 module HttpAcceptLanguage
-  class Railtie < ::Rails::Railtie
+  class Railtie < Rails::Railtie
 
     initializer "http_accept_language.add_middleware" do |app|
       app.middleware.use Middleware
